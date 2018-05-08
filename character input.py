0111cfb14c03e25b86cbc5f6c5a4a068c7ac5b68
Abhishek1abhishek <abu.mtech@gmail.com>
2018-05-08 07:19:23 +0545

    Character input of practicepython.com

---
+++ character input.py
@@ -0,0 +1,14 @@
+data = 0
+for i in range(0,101):
+    print()
+    print()
+    data += 1 
+    print("data %d in 100 data" %data)
+    name = str(input("Give me your name:"))
+    age = int(input("Give me your age: "))
+    q= (2018-age)+100
+    print("Your name is: ",name)
+    print("Your age is: ",age)
+    print(name + " after %d year you will become 100 years old" %q )
+    
+    

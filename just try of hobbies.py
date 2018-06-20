0f25ef5d8e93768b49fb3f083c23f8bd73fe57fe
Abhishek1abhishek <abu.mtech@gmail.com>
2018-06-20 06:55:49 +0545

    No need to check all this.
    All these programs are already submitted

---
+++ just try of hobbies.py
@@ -0,0 +1,9 @@
+hobbies = []
+
+# Add your code below!
+
+for num in range(3):
+  hobby =  str(input("Tell me one of your favorite hobbies: "))
+  hobbies.append(hobby)
+  hobbies.join(hobbies)
+print (hobbies)
--- new.py
+++
@@ -1,4 +0,0 @@
-a= input("enter any number:")
-a = int(a)
-print("The square number is:",a*a)
-print("the cube number is:",a*a*a)

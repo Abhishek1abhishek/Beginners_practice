d371d191e3dd8310e094ae620a79ec81ee25fab5
Abhishek1abhishek <abu.mtech@gmail.com>
2018-07-01 06:55:31 +0545

    MIT assignment 1

---
+++ Assignment 1.py
@@ -0,0 +1,14 @@
+import math
+X = input("enter a number in X")
+Y = input("Enter a number in Y")
+X= int(X)
+Y= int(Y)
+print(X**Y)
+print(math.log(X))
+
+import numpy
+x=(numpy.log(8))
+x= int(x)
+print (x)
+
+
